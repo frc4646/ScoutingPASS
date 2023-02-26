5f6d4cfc18042b4636451a0c85c6d2275e88d102
Andrew Vetter <andrewVetter@ScientificPineapple.com>
2023-02-26 17:09:24 -0600

    add Andrew's TBA auth key

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "sG8cdMe7c1nmtuABJAWeIyBqNPmx3JycGqng7szLKzfcQIpXc3XUkfjj28KMsjMZ";
 /**
  * Get list of teams in event
  *
