c86464b0d19ef36a53caa9e63e3ef3248e67ad47
Andrew Vetter <andrewVetter@ScientificPineapple.com>
2023-02-26 18:47:30 -0600

    make title 4646 specifc

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -1,7 +1,7 @@
 var config_data = `
 {
   "title": "Scouting PASS 2023",
-  "page_title": "Charged Up",
+  "page_title": "4646 Scouting app",
   "checkboxAs": "10",
   "prematch": [
     { "name": "Scouter Initials",
