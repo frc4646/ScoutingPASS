d51888a9ba7ee2dc26de51df9ed6beaeeb775f09
Theresa L. <33504766+tli228@users.noreply.github.com>
2025-01-27 18:52:35 -0600

    Experimenting

--- resources/js/googleSheets.js
+++ resources/js/googleSheets.js
@@ -1,26 +1,26 @@
 function setUpGoogleSheets() {
-    const scriptURL = '<SCRIPT URL>'
-    const form = document.querySelector('#scoutingForm')
-    const btn = document.querySelector('#submit')
- 
-    
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      btn.disabled = true
-      btn.innerHTML = "Sending..."
+  const scriptURL = '<SCRIPT URL>'
+  const form = document.querySelector('#scoutingForm')
+  const btn = document.querySelector('#submit')
 
-      let fd = getData(false)
-      for (const [key, value] of fd) {
-        console.log(`${key}: ${value}\n`);
-      }
+  
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    btn.disabled = true
+    btn.innerHTML = "Sending..."
 
-      fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: fd })
-        .then(response => { 
-              alert('Success!', response) })
-        .catch(error => {
-              alert('Error!', error.message)})
+    let fd = getData(false)
+    for (const [key, value] of fd) {
+      console.log(`${key}: ${value}\n`);
+    }
 
-      btn.disabled = false
-      btn.innerHTML = "Send to Google Sheets"
-    })
+    fetch(scriptURL, { method: 'POST', mode: 'no-cors', body: fd })
+      .then(response => { 
+            alert('Success!', response) })
+      .catch(error => {
+            alert('Error!', error.message)})
+
+    btn.disabled = false
+    btn.innerHTML = "Send to Google Sheets"
+  })
 }
